7ed32a0e18a140b4f299d26b3c265c470eb86b97
Will Sherman <vvj5@icloud.com>
2015-06-03 16:11:52 -0500

    Will display all todos as JSON when given URL

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,11 @@
-class TodoController < ApplicationController
+class TodosController < ApplicationController
 
   def index
+      todo = Todo.all
+      todo = todo.map do |todo|
+        "#{todo[:entry]}"
+      end
+      render json: "#{todo}", status: 200
   end
 
   def new
@@ -13,6 +18,39 @@ class TodoController < ApplicationController
   end
 
 end
+
+
+
+
+
+
+
+ # def puts_all_users
+ #      user = User.all
+ #      all_users = user.map do |user|
+ #        "#{user[:first_name]} #{user[:last_name]} Age #{user[:age]} #{params.inspect}"
+ #      end
+ #      render text: all_users.join("</br></br>"), status: 200
+ #    end
+
+ #    def puts_one_user
+ #      if User.exists?(params[:id])
+ #      user = User.find(params[:id])
+ #      render text: "#{user.first_name}", status: 200
+ #      else render text: "404 User Not Found", status: 404
+ #      end
+ #    end
+
+
+
+
+# Given the url http://localhost:3000/users?first_name=s
+# I should see ALL users from the database where first_name starts with s.
+
+
+
+
+
 
 
 
